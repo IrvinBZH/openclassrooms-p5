19de1f553a2d58a052b1ef62d30c609fe1949b59
IrvinBZH <IrvinP_Pro@outlook.fr>
2025-01-31 09:15:42 +0100

    All Javascript rework

--- assets/script.js
+++ assets/script.js
@@ -16,80 +16,88 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+ 
+'use strict';
+const numberOfImages = slides.length;
+const bannerElement = document.querySelector("#banner");
+const arrowRight = document.querySelector("#banner .arrow_right");
+const arrowLeft = document.querySelector("#banner .arrow_left");
 
-// PREVIEW
-const banner = document.getElementById("banner");
-let view = 0;
-
-const slide = document.createElement("img");
-slide.src = `assets/images/slideshow/${slides[view].image}`;
-slide.classList.add("banner-img");
-banner.appendChild(slide);
-const bannerTitle = document.createElement("p");
-bannerTitle.innerHTML = slides[view].tagLine;
-banner.appendChild(bannerTitle);
-
-// ARROWS
-
-const arrowLeft = document.querySelector(".arrow_left");
-	arrowLeft.addEventListener("click", () =>{
-		directionClick("left");
-});
-const arrowRight = document.querySelector(".arrow_right");
-	arrowRight.addEventListener("click", () =>{
-		directionClick("right");
-});
-
-
-function directionClick(arrow){
-	selectedDot.classList.remove("dot_selected");
-	switch (arrow) {
-		case "left":
-				view--;
-				if (view < 0){
-						view = slides.length -1;
-				}
-			break;
-		case "right":
-				view++;
-				if (view > slides.length - 1){
-					view = 0;	
-				}
-			break;
+// HTML Elements inside banner
+const createElements = (numberOfImages) =>{
+	bannerElement.insertAdjacentHTML('beforeend', `<p></p><div class="dots"></div>`);
+	const dotsElement = document.querySelector("#banner .dots");
+	for(let i=0; i<numberOfImages;i++){
+		dotsElement.insertAdjacentHTML('beforeend', `<div data-id="${i}" class="dot"></div>`);
 	}
-	slide.src = `assets/images/slideshow/${slides[view].image}`;
-	bannerTitle.innerHTML = slides[view].tagLine;
-	selectedDot = document.querySelector(`.dot${view}`);
-	selectedDot.classList.add("dot_selected"); 
-
-}
-// DOTS
-const bulletPoints = document.querySelector(".dots");
-
-for (let i = 0; i < slides.length; i++){
-	let dot = document.createElement("div");
-	dot.classList.add("dot")
-	dot.classList.add(`dot${i}`)
-	bulletPoints.appendChild(dot)
-
 }
 
-let selectedDot = document.querySelector(`.dot${view}`);
-	selectedDot.classList.add("dot_selected");
+// Display img banner, text, dot
+const displayBanner = (imageNumber=0) =>{
 
-// DOTS click
+	if (bannerElement.firstElementChild.classList.contains('banner-img')){
+		bannerElement.firstElementChild.src = `./assets/images/slideshow/${slides[imageNumber].image}`;
+	} else {
+		const imageBanner = `<img class="banner-img fade" src="./assets/images/slideshow/${slides[imageNumber].image}" alt ="Banner Print-it">`;
+		bannerElement.insertAdjacentHTML('afterbegin', imageBanner);
+	}
 
-function dotClick (select) {
-	selectedDot.classList.remove(`dot_selected`);
-	slide.src = `assets/images/slideshow/${slides[select].image}`;
-	bannerTitle.innerHTML = slides[select].tagLine;
-	selectedDot = document.querySelector(`.dot${select}`);
-	selectedDot.classList.add("dot_selected"); 
-}
+	// Text
+	const textBanner = `${slides[imageNumber].tagLine}`;
+	bannerElement.querySelector('p').innerHTML = textBanner;
 
-for (let i = 0; i < slides.length; i++){
-	let dot = document.querySelector(`.dot${i}`)
-	dot.addEventListener("click", () =>{
-	dotClick(i);
+	// Dots
+	const dots = document.querySelectorAll('#banner .dot');
+	dots.forEach((element, index) => {
+		if(index === imageNumber){
+			element.classList.add("dot_selected");
+		} else {
+			element.classList.remove("dot_selected")
+		}
 	});
+
+	return imageNumber;
+}
+
+// Click on dots
+const dotsHandler = () => {
+	const dotsElements = document.querySelectorAll("#banner .dot");
+
+	for (let dot of dotsElements){
+		dot.style.cursor = 'pointer';
+		dot.addEventListener('click', (e) => {
+			const thisDot = e.target;
+			const idDot = Number(thisDot.dataset.id);
+			displayBanner(idDot)
+		})
+	}
+}
+
+///
+createElements (numberOfImages);
+///
+let imageNumber = displayBanner();
+///
+dotsHandler();
+///
+
+// Arrow Events
+arrowLeft.addEventListener('click', (e) =>{
+	imageNumber--;
+	if(imageNumber < 0){
+		imageNumber = numberOfImages -1;
+	}
+	// display banner
+	bannerHandler(imageNumber);
+});
+arrowRight.addEventListener('click', (e) =>{
+	imageNumber++;
+	if(imageNumber >= numberOfImages){
+		imageNumber = 0;
+	}
+	// display banner
+	bannerHandler(imageNumber);
+});
+const bannerHandler = (imageNumber)=>{
+	displayBanner(imageNumber);
 }
